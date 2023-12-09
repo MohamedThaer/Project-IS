1eb7b28cb713d17eb3e919b48c8dc9e505273b8d
Mohammedhsbo <109453100+Mohammedhsbo@users.noreply.github.com>
2023-12-09 17:16:43 +0200

    Add files via upload

--- Project/src/project/Login.java
+++ Project/src/project/Login.java
@@ -172,7 +172,7 @@ public class Login extends javax.swing.JFrame {
     private void btn_LoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_LoginActionPerformed
         if (edt_text.getText().equals("admin") && edt_pass.getText().equals("12345")) {
             JOptionPane.showMessageDialog(this, "Login Success", "confirmation", 1);//where , message ,title
-            NewJFrame obj = new NewJFrame();
+            features obj = new features();
             dispose();
             obj.setVisible(true);
         } else {
